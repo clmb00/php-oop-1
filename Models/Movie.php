e35b923c5072fa8090bdf52a487524a6cd6121d7
clmb00 <paolo.colombo2000@gmail.com>
2022-12-07 17:10:56 +0100

    [add] genres as array

--- Models/Movie.php
+++ Models/Movie.php
@@ -3,22 +3,28 @@
 class Movie{
   public $title;
   public $year;
-  public $genre;
+  public $genres;
   public $descr;
   public $rating = '-';
 
   /**
    * @param String $_title The title of the movie
    * @param Number $_year The year of the releasing
-   * @param String $_title The genre of the movie
+   * @param Array $_title The genre of the movie
    * @param String $_title A little description/summary of the movie
    */
-  public function __construct($_title, $_year, $_genre, $_descr = "")
+  public function __construct($_title, $_year, $_genres, $_descr = "")
   {
     $this->title = $_title;
     $this->year = $_year;
-    $this->genre = $_genre;
     $this->descr = $_descr;
+
+    if(is_array($_genres)){
+      $this->genres = $_genres;
+    } else {
+      $this->genres = [''];
+      var_dump('Error');
+    }
   }
 
   public function setRating($my_rating){
@@ -27,10 +33,20 @@ class Movie{
     }
   }
 
+  public function getGenreString(){
+    $genres_string = $this->genres[0];
+    foreach($this->genres as $key => $genre){
+      if ($key){
+        $genres_string .= ", $genre";
+      }
+    }
+    return $genres_string;
+  }
+
   public function getFullInfo(){
-    return "<p><strong>Title</strong>: $this->title - <strong>Year</strong>: $this->year: </p> 
+    return "<p><strong>Title</strong>: $this->title - <strong>Year</strong>: $this->year </p> 
             <p><strong>Summary</strong>: $this->descr </p> 
-            <p><strong>Genre</strong>: $this->genre <strong>Rating</strong>: $this->rating / 10</p><hr>";
+            <p><strong>Genre</strong>: " . $this->getGenreString() ." <strong>Rating</strong>: $this->rating / 10</p><hr>";
   }
 
 }
--- index.php
+++ index.php
@@ -4,13 +4,13 @@ include_once './Models/Movie.php';
 
 $list_movies = [];
 
-$list_movies[] = new Movie('GoldeEye', 1995, 'Action', 'Years after a friend and fellow 00 agent is killed on a joint mission, a Russian crime syndicate steals a secret space-based weapons program known as "GoldenEye" and James Bond has to stop them from using it.');
+$list_movies[] = new Movie('GoldeEye', 1995, ['Action', 'Adventure', 'Thriller'], 'Years after a friend and fellow 00 agent is killed on a joint mission, a Russian crime syndicate steals a secret space-based weapons program known as "GoldenEye" and James Bond has to stop them from using it.');
 
-$list_movies[] = new Movie('The Fast and The Furious', 2001, 'Action', "Los Angeles police officer Brian O'Conner must decide where his loyalty really lies when he becomes enamored with the street racing world he has been sent undercover to destroy.");
+$list_movies[] = new Movie('The Fast and The Furious', 2001, ['Action', 'Crime', 'Thriller'], "Los Angeles police officer Brian O'Conner must decide where his loyalty really lies when he becomes enamored with the street racing world he has been sent undercover to destroy.");
 
-$list_movies[] = new Movie('Fight Club', 1999, 'Drama', "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.");
+$list_movies[] = new Movie('Fight Club', 1999, ['Drama'], "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.");
 
-$list_movies[] = new Movie('Inglourious Basterds', 2009, 'Adventure', "In Nazi-occupied France during World War II, a plan to assassinate Nazi leaders by a group of Jewish U.S. soldiers coincides with a theatre owner's vengeful plans for the same.");
+$list_movies[] = new Movie('Inglourious Basterds', 2009, ['Adventure', 'Drama', 'War'], "In Nazi-occupied France during World War II, a plan to assassinate Nazi leaders by a group of Jewish U.S. soldiers coincides with a theatre owner's vengeful plans for the same.");
 
 $list_movies[0]->setRating(8.7);
 
